e08d8e688acc36ebc87cf4e0b70498836920e0fd
Tai Jones <69367619+ProgramJones@users.noreply.github.com>
2024-05-22 17:18:10 -0500

    Added exercises for the 'data types' chapter.

---
+++ using_python/data_types/exercise_three.py
@@ -0,0 +1,9 @@
+pets = {
+    "Marcus": "Dog",
+    "Buster": "Dog",
+    "Rufus": "Dog",
+    "Fido": "Dog",
+    "Darwin": "Cat"
+}
+
+print(pets)
---
+++ using_python/data_types/exercise_two.py
@@ -0,0 +1,9 @@
+pet_names = (
+    "Marcus",
+    "Buster",
+    "Rufus",
+    "Fido",
+    "Darwin",
+)
+
+print(pet_names)
